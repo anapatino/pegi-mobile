93f2293b7c4a199245ca3cd46c89134a26218bfc
alciver1929 <unialciver29@gmail.com>
2022-11-25 11:47:03 -0500

    errores

--- lib/ui/pages/Consultar/estudiante/consultarPropuesta.dart
+++ lib/ui/pages/Consultar/estudiante/consultarPropuesta.dart
@@ -82,13 +82,7 @@ class _ConsultarPropuestasState extends State<ConsultarPropuestas> {
                           ? Color.fromARGB(255, 0, 0, 0)
                           : const Color.fromRGBO(18, 180, 122, 1),
                   colorBoton: const Color.fromRGBO(30, 30, 30, 1),
-                  onPressed: () {
-<<<<<<< HEAD
-                    //  Get.to(() => const MostrarPropuesta());
-=======
-                    //Get.to(() => const MostrarPropuesta());
->>>>>>> f2775eba4741e40c5affe28bca37b0a3cfc577f7
-                  });
+                  onPressed: () {});
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
